5200c609ecabefe551667a17e5d64cfe146c30d1
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-08-16 04:56:54 -0300

    feat: create folder and organize

---
+++ src/app/Home/index.tsx
@@ -0,0 +1,14 @@
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.tsx</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
