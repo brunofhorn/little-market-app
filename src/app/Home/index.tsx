059e5b4218182a3386b93a3d0127fc4e76678201
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-08-17 02:21:18 -0300

    feat: add filter and filter status

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,7 +1,11 @@
-import { Image, View } from 'react-native';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
+import { Filter } from '@/components/Filter';
+import { FilterStatus } from '@/types/FilterStatus';
+
+const FILTER_STATUS: FilterStatus[] = [FilterStatus.PENDING, FilterStatus.DONE]
 
 export function Home() {
   return (
@@ -13,7 +17,14 @@ export function Home() {
         <Button title="Adicionar" />
       </View>
 
-      <View style={styles.content}></View>
+      <View style={styles.content}>
+        <View style={styles.header}>
+          {FILTER_STATUS.map((status) => <Filter key={status} status={status} isActive />)}
+          <TouchableOpacity style={styles.clearButton}>
+            <Text style={styles.clearText}>Limpar</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
