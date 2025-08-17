61348cb4529015498e2e7a4ac7bd0fe55726818e
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-08-17 01:56:24 -0300

    feat: add input

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,13 +1,19 @@
 import { Image, View } from 'react-native';
 import { styles } from './styles';
 import { Button } from '@/components/Button';
+import { Input } from '@/components/Input';
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require('@/assets/logo.png')} style={styles.logo} />
 
-      <Button title="Adicionar"/>
+      <View style={styles.form}>
+        <Input placeholder='O que você precisa comprar?' />
+        <Button title="Adicionar" />
+      </View>
+
+      <View style={styles.content}></View>
     </View>
   );
 }
