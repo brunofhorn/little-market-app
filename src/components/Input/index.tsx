7fa6f38eace8414bb810543c7a553af0d3fb950f
Bruno Fernandes Horn <b.fernandeshorn@gmail.com>
2025-08-17 03:04:39 -0300

    feat: add list item

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -3,6 +3,10 @@ import { styles } from "./styles";
 
 export function Input({...rest}: TextInputProps){
     return (
-        <TextInput {...rest} style={styles.container} />
+        <TextInput 
+        style={styles.container}
+        placeholderTextColor="#74798B" 
+        {...rest} 
+        />
     )
 }
